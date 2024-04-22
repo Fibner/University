6baf589b733b4190e4ee480e6083f14180ebce99
Fibner <balagakontakt@gmail.com>
2024-04-23 00:08:49 +0200

    Experimenting with binary files

--- c_cpp/Semester_2/First_Project/First_Project/Source.cpp
+++ c_cpp/Semester_2/First_Project/First_Project/Source.cpp
@@ -9,6 +9,12 @@ public:
 	string brand;
 	string model;
 	int power;
+	Motorbike() {};
+	Motorbike(string _brand, string _model, int _power) {
+		brand = _brand;
+		model = _model;
+		power = _power;
+	};
 };
 
 class Customer {
@@ -19,16 +25,24 @@ public:
 };
 
 int main() {
-	int testNumber = 2;
-	int readTestNumber;
+	Motorbike motorbike1("Suzuki", "Katana", 999);
+	Motorbike motorbike2("Suzuki", "GSX-8R", 1135);
+	Motorbike motorbike3("Harley Davison", "Fatboy", 1868);
+	Motorbike motorbikes[3];
+	motorbikes[0] = motorbike1;
+	motorbikes[1] = motorbike2;
+	motorbikes[2] = motorbike3;
+
 	ofstream fileOut;
 	ifstream fileIn;
-	fileOut.open("test", ios::binary);
-	fileOut.write((char *) &testNumber, sizeof(int));
+	fileOut.open("motorbikes", ios::binary);
+	fileOut.write((char *) &motorbikes, sizeof(Motorbike)*3);
 	fileOut.close();
 
-	fileIn.open("test", ios::binary);
-	fileIn.read((char *)&readTestNumber, sizeof(int));
-	cout << readTestNumber << endl;
+	Motorbike readMotorbikes[3];
+	fileIn.open("motorbikes", ios::binary);
+	fileIn.read((char *) &readMotorbikes, sizeof(Motorbike)*3);
 	fileIn.close();
+	
+	return 0;
 }
